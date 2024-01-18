7bcf147b2ec1357dfb44095db52efbd5ee5b5157
SamehArmouche <sa74@alu.ua.es>
2024-01-18 09:50:52 +0100

    add 404 page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -3,12 +3,16 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import myImg from '../../assets/images/404.png';
+import colors from '../../assets/theme/colors';
 import { useTranslation } from 'react-i18next';
 
 function NotFound() {
   const { t } = useTranslation();
   return (
-    <Box sx={{height:'75.5%',alignItems:'center',justifyContent:'center',display:'flex'}}>
+    <Box sx={{height:'75.5%',alignItems:'center',justifyContent:'center',display:'flex',flexDirection:'column'}}>
+      <img src = {myImg}  alt={"notfound.png"} style={{width: 420,height:420}}></img>  
+      <h1 style={{color:colors.primary}}>{t('msg.404')}</h1>
     </Box>
   );
 }
