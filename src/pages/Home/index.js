b3a58cc58bc4ddadfec00761f64d49f694b11ad2
SamehArmouche <sa74@alu.ua.es>
2024-01-18 16:11:30 +0100

    add animation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
+import {Box, Typography, Grid, Slide, Button} from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 function Home() {
@@ -10,19 +7,20 @@ function Home() {
 
   return (
     <Box sx={{height:'75.5%',alignItems:'center',justifyContent:'center',display:'flex'}}>
+      <Slide direction={i18n.language==="ar"?"left":"right"} in={true} mountOnEnter unmountOnExit>
       <Grid container sx={{margin:0,p:{xs:0,md:0},width:"80%"}}>
         <Grid item xs={7} sx={{p:0}}>
           <Typography textAlign={i18n.dir()==="rtl"?"right":"left"} sx={{fontSize:{xs:18,md:30}}}>
-          {t('home.title')}
+            {t('home.title')}
           </Typography>
         </Grid>
         <Grid item xs={8} sx={{p:0}}>
-        <Button variant="home"
-        >
-          {t('button.joinUs')}
-        </Button>
+          <Button variant="home">
+            {t('button.joinUs')}
+          </Button>
         </Grid>
       </Grid>
+      </Slide>
     </Box>
   );
 }
--- src/pages/Talent/index.js
+++ src/pages/Talent/index.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import {Box, Typography, Grid, Slide} from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 function Talent() {
@@ -9,15 +7,17 @@ function Talent() {
 
   return (
     <Box sx={{height:'75.5%',alignItems:'center',justifyContent:'center',display:'flex'}}>
-      <Grid container sx={{margin:0,p:{xs:0,md:0},width:"80%"}}>
-        <Grid item xs={7} sx={{p:0}}>
-          <Typography textAlign={i18n.dir()==="rtl"?"right":"left"} sx={{fontSize:{xs:18,md:30}}}>
-          {t('talent.title')}
-          </Typography>
+      <Slide direction={i18n.language==="ar"?"left":"right"} in={true} mountOnEnter unmountOnExit>
+        <Grid container sx={{margin:0,p:{xs:0,md:0},width:"80%"}}>
+          <Grid item xs={7} sx={{p:0}}>
+            <Typography textAlign={i18n.dir()==="rtl"?"right":"left"} sx={{fontSize:{xs:18,md:30}}}>
+              {t('talent.title')}
+            </Typography>
+          </Grid>
+          <Grid item xs={8} sx={{p:0}}>
+          </Grid>
         </Grid>
-        <Grid item xs={8} sx={{p:0}}>
-        </Grid>
-      </Grid>
+      </Slide>
     </Box>
   );
 }
