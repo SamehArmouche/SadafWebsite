a08dcb4d9480b7d66679d87c1684e104754c3dca
SamehArmouche <sa74@alu.ua.es>
2024-01-19 09:35:17 +0100

    fixing name of website & height lang hmenu

--- src/components/shared/Header/index.js
+++ src/components/shared/Header/index.js
@@ -45,7 +45,7 @@ const CssMenu = styled((props: MenuProps) => (
   '& .MuiPaper-root': {
     borderRadius: 6,
     marginTop: theme.spacing(1),
-    width: type==="lang"?"20%":"100%",
+    width: type==="lang"?120:"100%",
     backgroundColor:'rgba(0, 0, 0,0.95)',
     boxShadow:
       'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
