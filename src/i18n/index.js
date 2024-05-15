a3403aa090166a69e2395107cfe9ceeaaeacb20e
SamehArmouche <sa74@alu.ua.es>
2024-05-15 13:11:19 +0200

    default lang

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
+const Languages = ['en', 'ar'];
 i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -9,6 +10,7 @@ i18n
   .init({
     debug: true,
     fallbackLng: 'en',
+    whitelist: Languages,
     react: { 
       useSuspense: false //   <---- this will do the magic
     }
